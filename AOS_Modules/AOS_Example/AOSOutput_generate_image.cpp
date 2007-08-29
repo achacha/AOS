55c4b79ee9b95b5197ed407141cfb35defe364c0
(no author) <(no author)@0cb227aa-09bc-7146-939c-0c54efedd0c0>
2007-08-29 21:59:02 +0000

    fixed for XML find
    git-svn-id: http://equinox:10080/_svn_root/AOS_Modules@9 0cb227aa-09bc-7146-939c-0c54efedd0c0

--- AOS_Modules/AOS_Example/AOSOutput_generate_image.cpp
+++ AOS_Modules/AOS_Example/AOSOutput_generate_image.cpp
@@ -17,14 +17,14 @@ bool AOSOutput_generate_image::execute(AOSOutputContext& context)
 {
   int x,y;
   AString str;
-  if (!context.getOutputParams().emitFromPath(ASW("/params/output/canvas/x", 23), str))
+  if (!context.getOutputParams().emitFromPath(ASW("output/canvas/x", 15), str))
   {
     m_Log.append("AOSOutput_generate_image: Unable to find '/params/output/canvas/x' parameter");
     ATHROW_EX(this, AException::InvalidParameter, "AOSOutput_generate_image requires '/params/output/canvas/x' parameter");
   }
   x = str.toInt();
   str.clear();
-  if (!context.getOutputParams().emitFromPath(ASW("/params/output/canvas/y", 23), str))
+  if (!context.getOutputParams().emitFromPath(ASW("output/canvas/y", 15), str))
   {
     m_Log.append("AOSOutput_generate_image: Unable to find '/params/output/canvas/y' parameter");
     ATHROW_EX(this, AException::InvalidParameter, "AOSOutput_generate_image requires '/params/output/canvas/y' parameter");
