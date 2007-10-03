35f0ec920a734a03ac1d5f6812ef7d6232cc4bff
achacha <achacha@2467b165-bbef-b140-816f-8cf3e0f08133>
2007-10-03 22:50:08 +0000

    Expanded the Lua test
    git-svn-id: http://equinox:10080/_svn_root/ALibraryImpl@13 2467b165-bbef-b140-816f-8cf3e0f08133

--- ALibraryImpl/ConsoleImpl/tLua.cpp
+++ ALibraryImpl/ConsoleImpl/tLua.cpp
@@ -1,13 +1,21 @@
 #include "AString.hpp"
 #include "ALuaEmbed.hpp"
 #include "AException.hpp"
+#include "AFile_AString.hpp"
 
 int main()
 {
   try
   {
+    AFile_AString strf(
+"\
+print '<b>Hello World</b><br/><small>';\n\
+alibrary.foo();\n\
+print '</small>';\n\
+");
+    
     ALuaEmbed lua(ALuaEmbed::LUALIB_ALL);
-    lua.execute(ASWNL("print \"Hello World\";\r\nalibrary.foo();\r\n"));
+    lua.execute(strf);
     std::cout << lua.useOutputBuffer() << std::endl;
   }
   catch(AException& ex)
