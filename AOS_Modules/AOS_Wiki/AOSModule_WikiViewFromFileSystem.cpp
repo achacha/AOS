ffb17a2a4b3ac266a5ab3823ed777d19557ec551
achacha <achacha@0cb227aa-09bc-7146-939c-0c54efedd0c0>
2007-11-08 14:46:21 +0000

    Almost working file wiki
    git-svn-id: http://equinox:10080/_svn_root/AOS_Modules@31 0cb227aa-09bc-7146-939c-0c54efedd0c0

--- AOS_Modules/AOS_Wiki/AOSModule_WikiViewFromFileSystem.cpp
+++ AOS_Modules/AOS_Wiki/AOSModule_WikiViewFromFileSystem.cpp
@@ -3,6 +3,7 @@
 #include "AFilename.hpp"
 #include "AFile_Physical.hpp"
 #include "AFileSystem.hpp"
+#include "ATextGenerator.hpp"
 
 const AString& AOSModule_WikiViewFromFileSystem::getClass() const
 {
@@ -30,17 +31,45 @@ bool AOSModule_WikiViewFromFileSystem::execute(AOSContext& context, const AXmlEl
   //a_Get relative wiki path
   AString str;
   context.useRequestParameterPairs().get(ASW("wikipath",8), str);
-  
   wikifile.join(str, false);
-  if (!AFileSystem::exists(wikifile))
+
+  AString strData;
+  if (context.useRequestParameterPairs().get(ASW("wiki.newdata",12), strData))
   {
-    //a_Signal that the wiki file does not exist
-    context.useOutputRootXmlElement().overwriteElement(ASW("wiki/DoesNotExits",17));
+    //a_New data submitted
+    AFilename newwikifile(wikifile);
+
+    //a_Temporary filename to use during the swap
+    AFilename tempwikifile(newwikifile);
+    tempwikifile.setExtension(ASW("_temporary_rename_",19));
+
+    //a_Generate temporary filename
+    AFileSystem::generateTemporaryFilename(newwikifile, ASW("_w_aoswiki",10));
+
+    //a_Save new data to temp filename
+    AFile_Physical file(newwikifile, "wb");
+    file.open();
+    file.write(strData);
+    file.close();
+
+
+    //a_Rename temp to current
+    AFileSystem::rename(wikifile, tempwikifile);
+    AFileSystem::rename(newwikifile, wikifile);
+    AFileSystem::remove(tempwikifile);
   }
   else
   {
-    AFile_Physical file(wikifile);
-    context.useOutputRootXmlElement().overwriteElement(ASW("wiki/row/data",13)).addData(file, AXmlElement::ENC_CDATADIRECT);
+    if (!AFileSystem::exists(wikifile))
+    {
+      //a_Signal that the wiki file does not exist
+      context.useOutputRootXmlElement().overwriteElement(ASW("wiki/DoesNotExits",17));
+    }
+    else
+    {
+      AFile_Physical file(wikifile);
+      context.useOutputRootXmlElement().overwriteElement(ASW("wiki/row/data",13)).addData(file, AXmlElement::ENC_CDATADIRECT);
+    }
   }
 
   return true;
