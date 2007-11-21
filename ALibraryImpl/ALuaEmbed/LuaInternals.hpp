dc460c720c58d151522c326f5a94a82487715ef6
achacha <achacha@2467b165-bbef-b140-816f-8cf3e0f08133>
2007-11-21 23:49:05 +0000

    Moved lua internals includes out
    git-svn-id: http://equinox:10080/_svn_root/ALibraryImpl@36 2467b165-bbef-b140-816f-8cf3e0f08133

---
+++ ALibraryImpl/ALuaEmbed/LuaInternals.hpp
@@ -0,0 +1,22 @@
+#ifndef DEFINED__LUAINTERNALS_HPP__
+#define DEFINED__LUAINTERNALS_HPP__
+
+/*!
+Functions needed by authors of Lua C functions
+*/
+extern "C"
+{
+#include "lua.h"
+
+/*!
+String appender utility functions used by ALibrary to override print()
+Output goes to the string buffer instead of the console
+*/
+LUA_API void (luaA_stringappender)(lua_State *L, const char *s);   /* appends a string (const char *) to ALuaEmbed output buffer, now also used by print() */
+
+#include "lauxlib.h"
+#include "lualib.h"
+#include "lstate.h"
+}
+
+#endif
