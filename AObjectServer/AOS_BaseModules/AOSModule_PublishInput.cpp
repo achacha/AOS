62666aa1a341cf234cb47c677ae7e562fa305238
achacha <achacha@1a28b8b1-d4e8-a449-a3f8-e3d44feecac6>
2007-12-16 15:02:52 +0000

    Lua fix
    git-svn-id: http://equinox:10080/_svn_root/AObjectServer@116 1a28b8b1-d4e8-a449-a3f8-e3d44feecac6

--- AObjectServer/AOS_BaseModules/AOSModule_PublishInput.cpp
+++ AObjectServer/AOS_BaseModules/AOSModule_PublishInput.cpp
@@ -15,8 +15,8 @@ AOSModule_PublishInput::AOSModule_PublishInput(AOSServices& services) :
 
 bool AOSModule_PublishInput::execute(AOSContext& context, const AXmlElement& params)
 {
-  context.useModel().addElement(ASW("input/request_header", 20), context.useRequestHeader());
-  context.useModel().addElement(ASW("output/response_header", 22), context.useResponseHeader());
+  context.useModel().addElement(ASW("input/request_header", 20)).addData(context.useRequestHeader(), AXmlElement::ENC_CDATADIRECT);
+  context.useModel().addElement(ASW("output/response_header", 22)).addData(context.useResponseHeader(), AXmlElement::ENC_CDATADIRECT);
 
   return true;
 }
