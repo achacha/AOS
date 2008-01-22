b43bdf898d0b508e074a70bb4bfa7678eac4e043
achacha <achacha@1a28b8b1-d4e8-a449-a3f8-e3d44feecac6>
2008-01-22 23:58:23 +0000

    Better Lua samples
    git-svn-id: http://equinox:10080/_svn_root/AObjectServer@143 1a28b8b1-d4e8-a449-a3f8-e3d44feecac6

--- AObjectServer/AObjectServer/aos_root/data/lua/TestLuaScript.lua
+++ AObjectServer/AObjectServer/aos_root/data/lua/TestLuaScript.lua
@@ -1,2 +1,2 @@
-model.addElementText("/root/inline-script-added", "Cthulhu was here");
-print(model.emitContentFromPath("/root/inline-script-added"));	
+model.setText("/root/inline-script-added", "Cthulhu was here");
+print(model.emitContentFromPath("/root/inline-script-added"));
