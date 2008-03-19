a45ab5d8993bb09f205464c4ac2203974c9b0a4b
achacha <achacha@2467b165-bbef-b140-816f-8cf3e0f08133>
2008-03-19 14:00:02 +0000

    openssl-0.9.8g
    git-svn-id: http://equinox:10080/_svn_root/ALibraryImpl@60 2467b165-bbef-b140-816f-8cf3e0f08133

--- ALibraryImpl/ACrypto/openssl/opensslconf.h
+++ ALibraryImpl/ACrypto/openssl/opensslconf.h
@@ -22,6 +22,12 @@
 #ifndef OPENSSL_NO_RFC3779
 # define OPENSSL_NO_RFC3779
 #endif
+#ifndef OPENSSL_NO_SEED
+# define OPENSSL_NO_SEED
+#endif
+#ifndef OPENSSL_NO_TLSEXT
+# define OPENSSL_NO_TLSEXT
+#endif
 
 #endif /* OPENSSL_DOING_MAKEDEPEND */
 #ifndef OPENSSL_NO_DYNAMIC_ENGINE
@@ -50,6 +56,12 @@
 # endif
 # if defined(OPENSSL_NO_RFC3779) && !defined(NO_RFC3779)
 #  define NO_RFC3779
+# endif
+# if defined(OPENSSL_NO_SEED) && !defined(NO_SEED)
+#  define NO_SEED
+# endif
+# if defined(OPENSSL_NO_TLSEXT) && !defined(NO_TLSEXT)
+#  define NO_TLSEXT
 # endif
 #endif
 
