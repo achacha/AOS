314ff0021db0960f143c1649bfeee3737c3e1b64
achacha <achacha@0cb227aa-09bc-7146-939c-0c54efedd0c0>
2008-07-15 18:47:19 +0000

    Better content
    git-svn-id: http://equinox:10080/_svn_root/AOS_Modules@110 0cb227aa-09bc-7146-939c-0c54efedd0c0

--- AOS_Modules/AOS_Example/AOSOutput_generate_image.cpp
+++ AOS_Modules/AOS_Example/AOSOutput_generate_image.cpp
@@ -42,6 +42,12 @@ AOSContext::ReturnCode AOSOutput_generate_image::execute(AOSContext& context)
     context.addError(getClass(), ASWNL("Unable to find '/output/fontpath' parameter"));
     return AOSContext::RETURN_ERROR;
   }
+  
+  //a_Get the text to paste onto image
+  AString strText;
+  context.useRequestParameterPairs().get(ASW("text",4), strText);
+  
+  
   AFilename filename(m_Services.useConfiguration().getAosBaseDataDirectory(), fontPath, false);
   fontPath.clear();
   filename.emit(fontPath);
@@ -54,9 +60,13 @@ AOSContext::ReturnCode AOSOutput_generate_image::execute(AOSContext& context)
   //a_Note that the silly C API actually wants (char *) even though (const char *) is more appropriate
   int brect[8] = { 0, 0, 0, 0, 0, 0, 0, 0};
   char *err;
-  if (err = gdImageStringTTF(canvas.useGdImagePtr(), brect, gdTrueColor(250,200,250), fontPath.startUsingCharPtr(), 12, 0.0, 20, 20, "This")) { context.addError(getClass(), AString(err)); return AOSContext::RETURN_ERROR; }
-  if (err = gdImageStringTTF(canvas.useGdImagePtr(), brect, gdTrueColor(250,250,200), fontPath.startUsingCharPtr(), 12, -0.1, 20, 40, "is a")) { context.addError(getClass(), AString(err)); return AOSContext::RETURN_ERROR; }
-  if (err = gdImageStringTTF(canvas.useGdImagePtr(), brect, gdTrueColor(200,250,250), fontPath.startUsingCharPtr(), 12, -0.2, 20, 60, "test!")) { context.addError(getClass(), AString(err)); return AOSContext::RETURN_ERROR; }
+  if (err = gdImageStringTTF(canvas.useGdImagePtr(), brect, gdTrueColor(220,100,220), fontPath.startUsingCharPtr(), 12, 0.0, 20, 20, "This")) { context.addError(getClass(), AString(err)); return AOSContext::RETURN_ERROR; }
+  if (err = gdImageStringTTF(canvas.useGdImagePtr(), brect, gdTrueColor(220,220,100), fontPath.startUsingCharPtr(), 12, -0.1, 20, 40, "is a")) { context.addError(getClass(), AString(err)); return AOSContext::RETURN_ERROR; }
+  if (err = gdImageStringTTF(canvas.useGdImagePtr(), brect, gdTrueColor(100,220,220), fontPath.startUsingCharPtr(), 12, -0.2, 20, 60, "test!")) { context.addError(getClass(), AString(err)); return AOSContext::RETURN_ERROR; }
+  
+  if (strText.getSize())
+    if (err = gdImageStringTTF(canvas.useGdImagePtr(), brect, gdTrueColor(250,250,250), fontPath.startUsingCharPtr(), 20, 0.0, 70, 50, strText.startUsingCharPtr())) 
+      { context.addError(getClass(), AString(err)); return AOSContext::RETURN_ERROR; }
 
   canvas.emitPNG(context.useOutputBuffer());
   context.useResponseHeader().setPair(AHTTPHeader::HT_ENT_Content_Type, ASW("image/png",9));
