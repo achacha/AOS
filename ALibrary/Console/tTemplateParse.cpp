332ab67e0d3231c7bbaeb50089742b0e1f1a6a47
achacha <achacha@1a8727a7-69a9-4645-974c-ee3eb180c2d6>
2008-08-29 21:32:05 +0000

    Fixed test case
    git-svn-id: http://equinox:10080/_svn_root/ALibrary@236 1a8727a7-69a9-4645-974c-ee3eb180c2d6

--- ALibrary/Console/tTemplateParse.cpp
+++ ALibrary/Console/tTemplateParse.cpp
@@ -1,12 +1,11 @@
+
 #include "ATemplate.hpp"
 #include "AFile_AString.hpp"
 #include "AFile_IOStream.hpp"
 #include "AEventVisitor.hpp"
-#include "AString.hpp"
-#include "AUrl.hpp"
 #include "AXmlElement.hpp"
 #include "AXmlDocument.hpp"
-#include "ANumber.hpp"
+#include "ABasePtrContainer.hpp"
 
 void testSimpleParse()
 {
@@ -14,14 +13,13 @@ void testSimpleParse()
 "Hello %[CODE]{{{\r\n print(/root/user/name); \r\n print(/root/user/loc); \r\n}}}[CODE]%!\
 ");
 
-  ABasePtrHolder objects;
+  ABasePtrContainer objects;
   AEventVisitor visitor;
   AXmlDocument model("root");
   model.useRoot().addElement(ASWNL("user/name"), ASWNL("Alex"));
   model.useRoot().addElement(ASWNL("user/loc"), ASWNL("(0,0)"));
 //    doc.debugDump();
   ATemplateContext ctx(objects, model, visitor);
-  objects.insert(ATemplate::OBJECTNAME_MODEL, pDoc, true);
 
   //a_Read template
   ATemplate tm;
@@ -29,27 +27,27 @@ void testSimpleParse()
 
   //a_Write it to cout
   AFile_IOStream iosfile;
-  std::cout << "---------------------debugDump---------------------" << std::endl;
-  tm.debugDump();
-  std::cout << "---------------------debugDump---------------------" << std::endl;
-  std::cout << "\r\n---------------------toAFile-----------------------" << std::endl;
-  tm.toAFile(iosfile);
-  std::cout << "---------------------toAFile-----------------------" << std::endl;
+  //std::cout << "---------------------debugDump---------------------" << std::endl;
+  //tm.debugDump();
+  //std::cout << "---------------------debugDump---------------------" << std::endl;
+  //std::cout << "\r\n---------------------toAFile-----------------------" << std::endl;
+  //tm.toAFile(iosfile);
+  //std::cout << "---------------------toAFile-----------------------" << std::endl;
   std::cout << "\r\n---------------------emit-----------------------" << std::endl;
   tm.emit(iosfile);
   std::cout << "---------------------emit-----------------------" << std::endl;
-  std::cout << "\r\n---------------------emitXml-----------------------" << std::endl;
-  AXmlElement xml;
-  tm.emitXml(xml);
-  xml.emit(iosfile,0);
-  std::cout << "---------------------emitXml-----------------------" << std::endl;
+  //std::cout << "\r\n---------------------emitXml-----------------------" << std::endl;
+  //AXmlElement xml;
+  //tm.emitXml(xml);
+  //xml.emit(iosfile,0);
+  //std::cout << "---------------------emitXml-----------------------" << std::endl;
 
   //a_Evaluate and emit to cout
   ARope rope;
-  tm.process(objects, rope);
+  tm.process(ctx, rope);
   std::cout << "\r\n---------------------process---------------------" << std::endl;
-  rope.toAFile(iosfile);
-  std::cout << "---------------------process---------------------" << std::endl;
+  rope.emit(iosfile);
+  std::cout << "---------------------emit---------------------" << std::endl;
 }
 
 int main()
