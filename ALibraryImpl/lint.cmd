03e75e09477b776a49fa1e540ae6db2ed49b0bb3
achacha <achacha@2467b165-bbef-b140-816f-8cf3e0f08133>
2008-09-02 20:53:11 +0000

    Lint command
    git-svn-id: http://equinox:10080/_svn_root/ALibraryImpl@97 2467b165-bbef-b140-816f-8cf3e0f08133

---
+++ ALibraryImpl/lint.cmd
@@ -0,0 +1,7 @@
+..\_devtools\lint\lint.py ACrypto -clean
+..\_devtools\lint\lint.py ADatabase_MySQL -clean
+..\_devtools\lint\lint.py ADatabase_ODBC -clean
+..\_devtools\lint\lint.py ADatabase_SQLite -clean
+..\_devtools\lint\lint.py AGdLib -clean
+..\_devtools\lint\lint.py ALuaEmbed -clean
+..\_devtools\lint\lint.py AZlib -clean
