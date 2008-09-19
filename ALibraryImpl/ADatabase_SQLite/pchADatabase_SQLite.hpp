0ef9d998141b773847215f8fe679f38fe03a970a
achacha <achacha@2467b165-bbef-b140-816f-8cf3e0f08133>
2008-09-19 22:10:26 +0000

    Some warning cleanup
    git-svn-id: http://equinox:10080/_svn_root/ALibraryImpl@99 2467b165-bbef-b140-816f-8cf3e0f08133

--- ALibraryImpl/ADatabase_SQLite/pchADatabase_SQLite.hpp
+++ ALibraryImpl/ADatabase_SQLite/pchADatabase_SQLite.hpp
@@ -6,10 +6,6 @@ $Id$
 #ifndef INCLUDED__pchADatabase_SQLite_HPP__
 #define INCLUDED__pchADatabase_SQLite_HPP__
 
-#if _MSC_VER > 1000
-#pragma once
-#endif // _MSC_VER > 1000
-
 #include "apiADatabase_SQLite.hpp"
 
 #include "AString.hpp"
--- ALibraryImpl/AGdLib/pchAGdLib.hpp
+++ ALibraryImpl/AGdLib/pchAGdLib.hpp
@@ -3,6 +3,17 @@ Written by Alex Chachanashvili
 
 $Id$
 */
+
+// Remove 4xxx warning that are part of 3rd party code (not going to stop warnings when compiling their source files)
+#if _MSC_VER > 1000
+  #pragma warning(disable:4244)  // conversion from 'foo' to 'bar', possible loss of data
+  #pragma warning(disable:4267)  // conversion from 'foo' to 'bar', possible loss of data
+  #pragma warning(disable:4306)  // conversion from 'foo' to 'bar' of greater size
+  #pragma warning(disable:4324)  // structure was padded due to __declspec(align())
+  #pragma warning(disable:4701)  // potentially uninitialized local variable 'foo' used
+  #pragma warning(disable:4996)  // This function or variable may be unsafe. Consider using XXXX_s instead. To disable deprecation, use _CRT_SECURE_NO_WARNINGS. See online help for details.
+#endif
+
 #include "apiABase.hpp"
 #include "apiAGdLib.hpp"
 #include "png.h"
