e4e5dd529ffa71e2f3e81b125638a1d7d3f9085b
achacha <achacha@1a28b8b1-d4e8-a449-a3f8-e3d44feecac6>
2008-10-17 21:00:48 +0000

    Linux scripts
    git-svn-id: http://equinox:10080/_svn_root/AObjectServer@293 1a28b8b1-d4e8-a449-a3f8-e3d44feecac6

---
+++ AObjectServer/call_make_aos_root.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+import os,sys;
+
+BASE_AOBJECTSERVER_PATH=os.path.dirname(os.path.abspath(sys.argv[0]));
+BASE_ENV_PATH=os.path.normpath(os.path.join(BASE_AOBJECTSERVER_PATH,"..","_devtools","bin"));
+
+params="";
+for param in sys.argv[1:]:
+  params = params + param + " ";
+
+os.chdir(BASE_ENV_PATH);
+os.system(os.path.join(BASE_ENV_PATH, "make_aos_root.py "+params));
