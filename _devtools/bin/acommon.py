f66444c066af901fdfd6239565a4c4ebfb00057f
achacha <achacha@5683aa36-efe6-e142-9988-6d92153951a4>
2008-10-17 21:01:31 +0000

    Linux scripts
    git-svn-id: http://equinox:10080/_svn_root/_devtools@9 5683aa36-efe6-e142-9988-6d92153951a4

--- _devtools/bin/acommon.py
+++ _devtools/bin/acommon.py
@@ -6,7 +6,8 @@ class Context:
   def __init__(self, devenv_bin):
     self.flags['win32'] = 0;
     self.flags['win64'] = 0;
-    self.flags['linux'] = 0;
+    self.flags['linux32'] = 0;
+    self.flags['linux64'] = 0;
     self.flags['clean'] = 0;
     self.flags['verbose'] = 0;
     self.flags['dryrun'] = 0;
@@ -16,6 +17,10 @@ class Context:
     self.BASE_ALIBRARYIMPL_PATH=os.path.join(self.BASE_CODE_PATH, "ALibraryImpl");
     self.BASE_AOBJECTSERVER_PATH=os.path.join(self.BASE_CODE_PATH, "AObjectServer");
     self.BASE_AOSMODULES_PATH=os.path.join(self.BASE_CODE_PATH, "AOS_Modules");
+    self.DEBUG_32 = os.path.join(self.BASE_CODE_PATH,"_debug");
+    self.RELEASE_32 = os.path.join(self.BASE_CODE_PATH,"_release");
+    self.DEBUG_64 = os.path.join(self.BASE_CODE_PATH,"_debug64a");
+    self.RELEASE_64 = os.path.join(self.BASE_CODE_PATH,"_release64a");
 
   def __str__(self):
     ret = "";
@@ -33,14 +38,48 @@ class Context:
     self.flags[name]=value;
 
 
-def syncPath(context, source_path, filemask, target_path):
+def syncPath(context, source_path, target_path):
+  EXEC_RSYNC_BASE="rsync -tuC --exclude=.svn ";
+  CMD = EXEC_RSYNC_BASE;
+  if (context.verbose == 1):
+    CMD = CMD + "-v ";
+  if (context.dryrun == 1):
+    CMD = CMD + "-n ";
+  if (context.recursive == 1):
+    CMD = CMD + "-r ";
+  CMD = CMD + source_path + " " + target_path;
+  if (context.verbose == 1):
+    print(os.getcwd()+":"+CMD);
+  os.system(CMD);
+def syncPathFiles(context, source_path, filemask, target_path):
   EXEC_RSYNC_BASE="rsync -tuC --exclude=.svn --exclude=pch*.hpp ";
   CMD = EXEC_RSYNC_BASE;
   if (context.verbose == 1):
     CMD = CMD + "-v ";
   if (context.dryrun == 1):
     CMD = CMD + "-n ";
+  if (context.recursive == 1):
+    CMD = CMD + "-r ";
   CMD = CMD + os.path.join(source_path,filemask) + " " + target_path;
   if (context.verbose == 1):
     print(os.getcwd()+":"+CMD);
   os.system(CMD);
+def syncFile(context, source_file, target_path):
+  EXEC_RSYNC_BASE="rsync -tuC ";
+  CMD = EXEC_RSYNC_BASE;
+  if (context.verbose == 1):
+    CMD = CMD + "-v ";
+  if (context.dryrun == 1):
+    CMD = CMD + "-n ";
+  CMD = CMD + source_file + " " + target_path;
+  if (context.verbose == 1):
+    print(os.getcwd()+":"+CMD);
+  os.system(CMD);
+def syncFileToOutput(context, source_file):
+  if (context.win32 == 1 or context.linux32 == 1):
+    syncFile(context, source_file, context.DEBUG_32);
+    syncFile(context, source_file, context.RELEASE_32);
+  if (context.win64 == 1 or context.linux64 == 1):
+    syncFile(context, source_file, context.DEBUG_64);
+    syncFile(context, source_file, context.RELEASE_64);
+
---
+++ _devtools/bin/make_aos_root.py
@@ -0,0 +1,117 @@
+#!/usr/bin/python
+#
+# Synchronize content
+#
+import os,sys;
+from acommon import *;
+
+def syncModule(context, module_source_path):
+  print("|----------------------------Content: "+module_source_path+" -> "+context.AOS_TARGET_PATH);
+  context.recursive = 1;
+  syncPath(context, module_source_path, context.BASE_CODE_PATH);
+  context.recursive = 0;
+
+def syncDocs(context):
+  source_path = os.path.join(context.BASE_AOBJECTSERVER_PATH, "docs");
+  target_path = os.path.join(context.AOS_TARGET_PATH, "docs");
+  print("|----------------------------Docs: "+source_path+" -> "+target_path);
+  context.recursive = 1;
+  syncPath(context, source_path, target_path);
+  context.recursive = 0;
+
+def sync_aos_root(context, base_path):
+  # Now process modules located in module_path
+  # (similar code can be added for other module paths by calling syncModule(...source path...)
+  if (context.verbose == 1):
+    print("sync_aos_root("+base_path+")");
+  if (os.path.exists(base_path)):
+    for module in os.listdir(base_path):
+      module_path = os.path.join(base_path, module);
+      if (os.path.isdir(module_path)):
+        if (module == "aos_root"):
+          syncModule(context, module_path);
+        else:
+          sync_aos_root(context, module_path);
+
+def showUsage():
+  print("AOS configuration and content synchronization script.");
+  print(sys.argv[0]+" [-win32] [-win64] [-linux] <-verbose|-dryrun|-clean|-makebear> <-p BASEPATH>");
+  print("  -win32          - Windows 32-bit target");
+  print("  -win64          - Windows 64-bit target");
+  print("  -linux32        - Linux 32-bit target");
+  print("  -linux64        - Linux 64-bit target");
+  print("  -v              - verbose mode");
+  print("  -clean          - removes previous version");
+  print("  -dryrun         - only show what is going to be copied, but do not copy");
+  print("  -makebare       - Create a bare version of aos_root in aos_root_bare");
+  print("  -p BASEPATH     - uses BASEPATH/_debug/ and BASEPATH/_release for deployment");
+  print("\r\nExample:");
+  print(" -v -p /output/dir/");
+  print(" -p /tmp -dryrun");
+
+######################################################################
+########################## MAIN ######################################
+######################################################################
+
+context = Context(os.getcwd());
+context.AOS_TARGET_PATH = os.path.join(context.BASE_CODE_PATH, "aos_root");
+
+argc = 1;
+while (len(sys.argv) > argc):
+  if (sys.argv[argc] == "-dryrun"):
+    context.dryrun = 1;
+  elif (sys.argv[argc] == "-v"):
+    context.verbose = 1;
+  elif (sys.argv[argc] == "-clean"):
+    context.clean = 1;
+  elif (sys.argv[argc] == "-win32"):
+    context.win32 = 1;
+  elif (sys.argv[argc] == "-win64"):
+    context.win64 = 1;
+  elif (sys.argv[argc] == "-linux32"):
+    context.linux32 = 1;
+  elif (sys.argv[argc] == "-linux64"):
+    context.linux64 = 1;
+  elif (sys.argv[argc] == "-p"):
+    context.AOS_TARGET_PATH = sys.argv[argc+1];
+    argc += 1;
+  else:
+    print("Unknown parameter: "+sys.argv[argc]);
+    showUsage();
+    sys.exit(-1);
+
+  argc += 1;
+
+if (context.win32 == 0 and context.win64 == 0 and context.linux32 == 0 and context.linux64 == 0):
+  print("Please select -win32 and/or -win64 and/or -linux32 and/or -linux64");
+  showUsage();
+  sys.exit(-1);
+
+# Now process modules located in ../AOS_Modules
+# (similar code can be added for other module paths by calling syncModule(...source path...)
+if (context.verbose == 1):
+  print(str(context));
+
+# Clean if needed
+if (context.clean == 1):
+  if (context.dryrun == 1):
+    print("Dry-run detected, would clean: "+os.path.join(target_path, "aos_root"));
+    print("Exiting, rest of dry run cannot be determined with clean enabled");
+    sys.exit(0);
+  else:
+    # Remove aos_root directory
+    print("Removing: "+context.AOS_TARGET_PATH);
+    os.system("rm -Ir "+ context.AOS_TARGET_PATH);
+
+# Copy extra needed stuff
+if (not os.path.exists(context.AOS_TARGET_PATH)):
+  os.mkdir(context.AOS_TARGET_PATH);
+
+# Sync aos_root directories
+sync_aos_root(context, context.BASE_AOBJECTSERVER_PATH);
+#sync_aos_root(context, context.BASE_AOSMODULES_PATH);
+#syncDocs(context);
+
+syncFileToOutput(context, os.path.join(context.BASE_AOBJECTSERVER_PATH, "openssl_create_selfsigned_certificate.py"));
+syncFileToOutput(context, os.path.join(context.BASE_AOBJECTSERVER_PATH, "openssl.config"));
+
