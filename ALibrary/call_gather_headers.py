b53ed3f73617871aa76465e752aae2ee92cc14ef
achacha <achacha@1a8727a7-69a9-4645-974c-ee3eb180c2d6>
2008-10-17 19:15:03 +0000

    Made script work on linux
    git-svn-id: http://equinox:10080/_svn_root/ALibrary@253 1a8727a7-69a9-4645-974c-ee3eb180c2d6

---
+++ ALibrary/call_gather_headers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+import os,sys;
+
+BASE_ALIBRARY_PATH=os.path.dirname(os.path.abspath(sys.argv[0]));
+#print("BASE_ALIBRARY_PATH="+BASE_ALIBRARY_PATH);
+BASE_ENV_PATH=os.path.normpath(os.path.join(BASE_ALIBRARY_PATH,"..","_devtools","bin"));
+
+params="";
+for param in sys.argv[1:]:
+  params = params + param + " ";
+
+os.chdir(BASE_ENV_PATH);
+print(os.getcwd());
+os.system("./gather_headers.py "+params);
