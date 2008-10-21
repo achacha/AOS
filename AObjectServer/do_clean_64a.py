876114276cf16139eee53c638b2a35589e7689a9
achacha <achacha@1a28b8b1-d4e8-a449-a3f8-e3d44feecac6>
2008-10-21 19:59:11 +0000

    Scripts and makefiles
    git-svn-id: http://equinox:10080/_svn_root/AObjectServer@298 1a28b8b1-d4e8-a449-a3f8-e3d44feecac6

--- AObjectServer/do_clean_64a.py
+++ AObjectServer/do_clean_64a.py
@@ -2,21 +2,23 @@
 #
 # Synchronize content
 #
+#!/bin/python
 import os,sys;
 
-THIS_PATH=os.path.dirname(sys.argv[0]);
-ALIBRARY_PATH=os.path.normpath(os.path.join(THIS_PATH,"..","ALibrary"));
-GATHER_SCRIPT=os.path.join(ALIBRARY_PATH,"call_gather_headers.py");
-MAKEAOS_SCRIPT=os.path.join(THIS_PATH, "call_make_aos_root.py");
+devenvbin = os.path.normpath(os.path.join(os.getcwd(), "..", "_devtools", "bin"));
+sys.path.append(devenvbin);
+from acommon import *;
 
-if (not os.path.exists(GATHER_SCRIPT)):
-  print "Gather headers script missing: "+GATHER_SCRIPT;
+context = Context(devenvbin);
+if (context.processArgv(sys.argv) == -1):
+  showUsage();
   sys.exit(-1);
 
-if (not os.path.exists(MAKEAOS_SCRIPT)):
-  print "Gather aos_root script missing: "+MAKEAOS_SCRIPT;
-  sys.exit(-1);
+context.win64 = 1;
+context.clean = 1;
+  
+# Gather headers
+context.executeScript("gather_headers.py");
 
-os.system(GATHER_SCRIPT+" -win64 -clean");
-os.system(MAKEAOS_SCRIPT+" -win64 -clean");
-
+# Make aos_root
+context.executeScript("make_aos_root.py");
