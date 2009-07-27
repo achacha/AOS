2a22c6bf43a506a7e6de5676d4c7884821ff479f
achacha <achacha@1a8727a7-69a9-4645-974c-ee3eb180c2d6>
2009-07-27 18:46:22 +0000

    Renamed variable to avoid name conflict
    git-svn-id: http://equinox:10080/_svn_root/ALibrary@294 1a8727a7-69a9-4645-974c-ee3eb180c2d6

--- ALibrary/UnitTests/ut_ACookie_General.cpp
+++ ALibrary/UnitTests/ut_ACookie_General.cpp
@@ -9,32 +9,32 @@ $Id$
 
 void testCookieJar(int& iRet)
 {
-  const AString DOMAIN("foo.nil");
+  const AString DOMAINNAME("foo.nil");
   ACookieJar cookieJar;
-  ACookie *pcookie = new ACookie("root0", "val", DOMAIN);
+  ACookie *pcookie = new ACookie("root0", "val", DOMAINNAME);
   pcookie->setSecure(true);
   cookieJar.add(pcookie);
-  cookieJar.add(new ACookie("dir1", "val", DOMAIN, "/dir"));
-  cookieJar.add(new ACookie("dir2", "val", DOMAIN, "/dir/folder/"));
+  cookieJar.add(new ACookie("dir1", "val", DOMAINNAME, "/dir"));
+  cookieJar.add(new ACookie("dir2", "val", DOMAINNAME, "/dir/folder/"));
 
   AString str;
-  cookieJar.emitCookies(str, DOMAIN, "/dir");
+  cookieJar.emitCookies(str, DOMAINNAME, "/dir");
   ASSERT_UNIT_TEST(str.equals("dir1=val; root0=val; "), "ACookieJar::emit", "0", iRet);
 
   str.clear();
-  cookieJar.emitCookies(str, DOMAIN, "/dir/folder");
+  cookieJar.emitCookies(str, DOMAINNAME, "/dir/folder");
   ASSERT_UNIT_TEST(str.equals("dir2=val; dir1=val; root0=val; "), "ACookieJar::emit", "1", iRet);
 
   str.clear();
-  cookieJar.emitCookies(str, DOMAIN, "/dir/folder2");
+  cookieJar.emitCookies(str, DOMAINNAME, "/dir/folder2");
   ASSERT_UNIT_TEST(str.equals("dir1=val; root0=val; "), "ACookieJar::emit", "2", iRet);
 
   str.clear();
-  cookieJar.emitCookies(str, DOMAIN, "/dir/folder2", true);
+  cookieJar.emitCookies(str, DOMAINNAME, "/dir/folder2", true);
   ASSERT_UNIT_TEST(str.equals("root0=val; "), "ACookieJar::emit", "3", iRet);
 
   str.clear();
-  cookieJar.emitCookies(str, DOMAIN, "/");
+  cookieJar.emitCookies(str, DOMAINNAME, "/");
   ASSERT_UNIT_TEST(str.equals("root0=val; "), "ACookieJar::emit", "4", iRet);
 }
 
