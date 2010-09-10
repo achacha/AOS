d455cfd9653d61dac4bb5075078bc31b0fcffdb7
achacha <achacha@1a8727a7-69a9-4645-974c-ee3eb180c2d6>
2010-09-10 21:47:25 +0000

    Thread pool unit test
    git-svn-id: http://equinox:10080/_svn_root/ALibrary@332 1a8727a7-69a9-4645-974c-ee3eb180c2d6

---
+++ ALibrary/UnitTests/ut_AThreadPool_General.cpp
@@ -0,0 +1,46 @@
+/*
+Written by Alex Chachanashvili
+
+$Id: ut_AStringHashMap_General.cpp 205 2008-05-29 13:58:16Z achacha $
+*/
+#include "pchUnitTests.hpp"
+#include "AThreadPool.hpp"
+#include "ASync_CriticalSection.hpp"
+
+class UTTPData : public ABase
+{
+public:
+  UTTPData() : counter(0) {}
+  ASync_CriticalSection sync;
+  int counter;
+};
+
+static u4 _threadprocUnitTestThreadPool(AThread& thread)
+{
+  UTTPData *pData = (UTTPData *)thread.getParameter();
+  thread.setRunning(true);
+
+  {
+    ALock lock(pData->sync);
+    ++(pData->counter);
+  }
+
+  thread.setRunning(false);
+  return 0;
+}
+
+int ut_AThreadPool_General()
+{
+  std::cerr << "ut_AThreadPool_General" << std::endl;
+
+  int iRet = 0x0;
+  UTTPData data;
+  AThreadPool pool(_threadprocUnitTestThreadPool, 10, NULL, &data);
+  pool.setCreatingNewThreads(false);
+  pool.setTotalThreadCreationCount(10);
+  pool.start();
+  pool.waitForThreadsToExit();
+
+  ASSERT_UNIT_TEST(data.counter == 10, "AThreadPool", "0", iRet);
+  return iRet;
+}
